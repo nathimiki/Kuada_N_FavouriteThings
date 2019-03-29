c1d6951ce63d87591f3f468b3b7a28c748eb6c94
nathimiki <nathimiki@gmail.com>
2019-03-28 23:06:24 -0400

    edited index and song, css and added images/videos

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    user: "root",
+    password: "",
+    port: 3306, //for mac its 8889
+    host: "localhost",
+    database: "db_favThings"
+}
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
+var connect = require('../utils/sqlConnect');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //get data from db and send through the route to the index.bhs view and then render
+  connect.query('SELECT song_name, artist_name, song_img FROM tbl_song', (error, result)=>{
+    if(error){
+      throw error;
+      console.log(error);
+    }else{
+      console.log(result);
+        res.render('index', { songs: result });
+    }
+  })
+});
+
+/* GET song page. */
+router.get('/:song', function(req, res, next) {
+  //get data from db and send through the route to the index.bhs view and then render
+  connect.query(`SELECT * FROM tbl_song WHERE song_name="${req.params.song}"`, (error, result)=>{
+    if(error){
+      throw error;
+      console.log(error);
+    }else{
+      console.log(result);
+        res.render('song', { songData: result[0] });
+    }
+  })
 });
 
 module.exports = router;
